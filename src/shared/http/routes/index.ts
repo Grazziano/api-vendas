875c43e917ae6d8f73f40323737bf7f05e695ad9
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-30 01:11:47 -0300

    Rotas de Produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World 😃!' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
