c95bbe19c8dfb4ad564df51c9b6855407b5f9102
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-07-06 13:21:38 -0300

    Rota estática para as imagens de avatar

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,17 +1,20 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import '../typeorm';
+
+import express, { NextFunction, Request, Response } from 'express';
+
+import AppError from '../errors/AppError';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes';
-import AppError from '../errors/AppError';
-import '../typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
