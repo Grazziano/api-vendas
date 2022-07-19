25cf412c3a574fcf7040afe7a875bc2494bb0209
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-07-19 13:48:40 -0300

    Conclusão: Envio de Email para Recuperação de Senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import EtherealMail from '@config/mail/EtherealMail';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
+import path from 'path';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokenRepository';
 
@@ -19,11 +20,14 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    // console.log(user);
-
     const token = await userTokenRepository.generate(user.id);
 
-    // console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await EtherealMail.sendMail({
       to: {
@@ -32,10 +36,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API Vendas] Recuperação de Senha',
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token: token.token,
+          link: `http://localhost:3000/reset_password?token=${token.token}`,
         },
       },
     });
